2041c5bfeea6ef566bbce17af748cee175866744
codersx <1743091664@qq.com>
2024-02-02 22:12:19 +0800

    chore: close empty func eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,10 +21,11 @@ module.exports = {
     "@typescript-eslint"
   ],
   "rules": {
-    '@typescript-eslint/no-var-requires': 'off', // 可以使用require导入
-    '@typescript-eslint/no-explicit-any': 'off', // 可以使用any类型
+    '@typescript-eslint/no-var-requires': 'off', // 可以使用 require 导入
+    '@typescript-eslint/no-explicit-any': 'off', // 可以使用 any 类型
     '@typescript-eslint/no-unused-vars': 'off', // 定义了却们没有使用的检查
-    'react/prop-types': 'off', // 关闭react的porpType检查
-    '@typescript-eslint/no-empty-interface': 'off' // 关闭interface不为空对象
+    'react/prop-types': 'off', // 关闭 react 的 porpType 检查
+    '@typescript-eslint/no-empty-interface': 'off', // 关闭 interface 不为空对象
+    '@typescript-eslint/no-empty-function': 'off' // 关闭空函数
   }
 }
